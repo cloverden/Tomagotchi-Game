bb6312c98e7f39ad2c341640d60f6963b7859e5e
Patricia Ceban <patricia.ceban.contact@gmail.com>
2025-06-19 18:49:19 -0400

    working on ui

--- public/ui.js
+++ public/ui.js
@@ -1,11 +1,32 @@
-var txt = document.getElementById('textbox'); 
-// txt.style.visbility = hidden; 
+// var txt = document.getElementById('textbox'); 
+// // txt.style.visbility = hidden; 
 
-const textArea = document.getElementById("txtarea"); 
-textArea.addEventListener('click', Name(), {
+// const textArea = document.getElementById("txtarea"); 
+// textArea.addEventListener('click', Name(), {
+// }); 
+//     function Name() {
+//         const name_val = textArea.value; 
+//         console.log(name_val); 
+//         // txt.style.visibility = visible; 
+//     }
+
+
+document.addEventListener("DOMContentLoaded", function() {
 }); 
+const textArea = document.getElementById("txtarea"); 
+
     function Name() {
         const name_val = textArea.value; 
         console.log(name_val); 
         // txt.style.visibility = visible; 
-    }
+    }; 
+document.addEventListener("DOMContentLoaded", Name); 
+
+
+const texter = document.getElementById("text"); 
+function dress() {
+    console.log("fired"); 
+    var info = "Dress Up Your Pet"; 
+    document.getElementById("text").innerText = info; 
+}; 
+
