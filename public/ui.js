263f994a10bdf9f02d7fa05f0c5e70314a551373
Patricia Ceban <patricia.ceban.contact@gmail.com>
2025-06-19 19:47:58 -0400

    visibility functions

--- public/ui.js
+++ public/ui.js
@@ -14,21 +14,44 @@
 document.addEventListener("DOMContentLoaded", function() {
 }); 
 const textArea = document.getElementById("txtarea"); 
+const textArea_b = document.getElementById("save_name"); 
 
     function Name() {
         const name_val = textArea.value; 
         sessionStorage.setItem("petName", name_val); //key, value
         console.log(name_val); 
-        // txt.style.visibility = visible; 
+        txt_tog(); 
+        txtarea_tog(); 
     }; 
 document.addEventListener("DOMContentLoaded", Name); 
 
+//textbox visibility 
+txtbox = document.getElementById("textbox"); 
+function txt_tog() {
+    if(txtbox.style.display == "none") {
+        txtbox.style.display = "block"; 
+    } else {
+        txtbox.style.display = "none"; 
+    }
+}
+
+//textarea visibility 
+function txtarea_tog()  {
+    if(textArea.style.display == "block" && textArea_b.style.display == "block") {
+        textArea.style.display = "none"; 
+        textArea_b.style.display = "none"; 
+    } else {
+        textArea.style.display = "block"; 
+        textArea_b.style.display = "block"; 
+    }
+}
 
 //icons
 const texter = document.getElementById("text"); 
 function dress_in() {
     console.log("di_fired"); 
     var info = "Dress up" + " " + sessionStorage.getItem("petName"); 
+    texter.style.display = "block"; 
     document.getElementById("text").innerText = info; 
 }; 
 
@@ -48,9 +71,10 @@ function mouse_out() {
     console.log("mo_fired"); 
     var info = "Text"; 
     document.getElementById("text").innerText = info; 
+    texter.style.display = "none"; 
 }
 
-//mouseout frame delay
+//^ frame delay
 if(mouse_out()) {
     setTimeout(4000); //1000 miliseconds = 1 second
 }
