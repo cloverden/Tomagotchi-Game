2c9c11d78d86acd8f155eb1ef7449863c41144b7
Patricia Ceban <patricia.ceban.contact@gmail.com>
2025-06-19 21:11:48 -0400

    cleaned up code also

--- public/ui.js
+++ public/ui.js
@@ -1,15 +1,3 @@
-// var txt = document.getElementById('textbox'); 
-// // txt.style.visbility = hidden; 
-
-// const textArea = document.getElementById("txtarea"); 
-// textArea.addEventListener('click', Name(), {
-// }); 
-//     function Name() {
-//         const name_val = textArea.value; 
-//         console.log(name_val); 
-//         // txt.style.visibility = visible; 
-//     }
-
 
 document.addEventListener("DOMContentLoaded", function() {
 }); 
